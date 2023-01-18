5780a235f3181c9e5b2694f8983e8932212ad997
Brandon <17382500+bmlui@users.noreply.github.com>
2023-01-18 15:48:54 -0500

    progress

--- project1/project-utilities/wcat.c
+++ project1/project-utilities/wcat.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	FILE *fp = fopen("foo.txt", "r"); 
+int main(int argc, char *argv[]) {
+ if ( argc < 2 )
+    {
+        printf( "Please enter a file name to open and print\n" );
+        return 1;  
+    }
+    FILE *fp = fopen(argv[1], "r"); 
+
 if (fp == NULL) { 
-puts("cannot open file\n");
+puts("Cannot open file\n");
 exit(1); }
+do{
+} while(TRUE);
+
 }
