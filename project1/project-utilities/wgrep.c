17d985c4539598c239dd6bad9ccde6ef3f2472f2
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-01-19 19:33:57 -0500

    pro 1 done

--- project1/project-utilities/wgrep.c
+++ project1/project-utilities/wgrep.c
@@ -1,1 +1,42 @@
-Place code here.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+ if ( argc < 2 ) {
+        printf("wgrep: searchterm [file ...]\n");
+        return 1;
+    }
+    int runNum = argc;
+    if (argc == 2) {
+         runNum = argc+1;
+    }
+    char* searchTerm = argv[1];
+    
+    for (int i = 2; i<runNum; i++) {
+        FILE *fp;
+        if (argc ==2 ) {
+           fp = stdin;
+        } else {
+            fp = fopen(argv[i], "r");
+        }
+            if (fp == NULL) {
+            printf("wgrep: cannot open file\n");
+            return(1); }
+                char s[10000] = "";
+                do {
+                    if ((strstr(s, searchTerm) != NULL) | (strcmp(searchTerm," ") == 0)) {
+                        printf("%s", s);
+                    }
+                }while(fgets(s, sizeof(s), fp));
+            
+            fclose(fp);
+    }
+   
+    
+    
+
+
+
+}
