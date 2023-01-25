ddeab489a647d33693f5714dcda9ec2a6e97a459
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-01-25 23:40:23 +0000

    p1

--- project2/project-processes/wshell.c
+++ project2/project-processes/wshell.c
@@ -4,38 +4,54 @@
 #include <string.h>
 #include <unistd.h>
 
-
-int main (int argc, char *argv[]) {
-    bool isRunning = true;
-    
-   
-while (isRunning)
+int main(int argc, char *argv[])
 {
-    char workingPath[100];
-     getcwd(workingPath, 100);
-    char* basename = strrchr(workingPath, '/');
-    basename++;
-    char input[100];
-    printf("%s$ ", basename );
-    fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
-    if (strcmp(input, "exit") == 0)
+    bool isRunning = true;
+
+    while (isRunning)
     {
-        isRunning = false;
-    } else if (strncmp(input, "cd", 2) == 0)
-    {
-        printf("%s", strcat(strcat(workingPath, "/"), input));
-        chdir(strcat(strcat(workingPath, "/"), input));
-    } else if (strncmp(input, "echo", 4) == 0)
-    {
-        printf("%s", input);
-    }
-    else
-    {
-       printf("Command not found");
+        char workingPath[100];
+        getcwd(workingPath, 100);
+        char *basename = strrchr(workingPath, '/');
+        basename++;
+        char input[100];
+        printf("%s$ ", basename);
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = '\0';
+        if (strncmp(input, "exit", 4) == 0)
+        {
+            isRunning = false;
+        }
+        else if (strncmp(input, "cd", 2) == 0)
+        {
+            char *val = input + 3;
+            if ((strcmp(input, "cd") == 0)| (strcmp(input, "cd ") == 0))
+            {
+                val = getenv("HOME");
+            }
+            else
+            {
+                if (strcmp(val, "..") == 0) {
+                    val = val;
+                } else if (strncmp(val, "/", 1) != 0)
+                {
+                    val = strcat(strcat(workingPath, "/"), val);
+                }  
+                chdir(val);
+            }
+        }
+        else if (strncmp(input, "echo", 4) == 0)
+        {
+            if (strncmp(input, "echo ", 5)  == 0 ) {
+            char * val = input +5; 
+            printf("%s\n", val);
+            }
+          
+        }
+        else
+        {
+            system(input);
+            printf("Command not found\n");
+        }
     }
 }
- 
-
-}
-
