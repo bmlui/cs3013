9629949b0e007531ff186f4decded76fce2d9c11
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-01-26 01:35:45 +0000

    part 1 done

--- project2/project-processes/wshell.c
+++ project2/project-processes/wshell.c
@@ -7,7 +7,6 @@
 int main(int argc, char *argv[])
 {
     bool isRunning = true;
-
     while (isRunning)
     {
         char workingPath[100];
@@ -15,9 +14,16 @@ int main(int argc, char *argv[])
         char *basename = strrchr(workingPath, '/');
         basename++;
         char input[100];
+         if (strcmp(basename, "") == 0) {
+            basename = "/";
+        }
         printf("%s$ ", basename);
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
+       if (!isatty(fileno(stdin))) { 
+            printf("%s\n", input);
+            fflush(stdout);
+             }
         if (strncmp(input, "exit", 4) == 0)
         {
             isRunning = false;
@@ -25,19 +31,27 @@ int main(int argc, char *argv[])
         else if (strncmp(input, "cd", 2) == 0)
         {
             char *val = input + 3;
-            if ((strcmp(input, "cd") == 0)| (strcmp(input, "cd ") == 0))
-            {
-                val = getenv("HOME");
-            }
+            char *dirname = input +3;
+             if (strstr(val, " ")) {
+                    printf("wshell: cd: too many arguments\n");
+                }
             else
             {
-                if (strcmp(val, "..") == 0) {
+                 if (strcmp(val, "..") == 0) {
                     val = val;
-                } else if (strncmp(val, "/", 1) != 0)
+                }  else if ((strcmp(input, "cd") == 0)| (strcmp(input, "cd ") == 0))
+            {
+                val = getenv("HOME");
+               
+            } else if (strncmp(val, "/", 1) != 0)
                 {
+                    dirname = val;
                     val = strcat(strcat(workingPath, "/"), val);
                 }  
-                chdir(val);
+                int rv = chdir(val);
+                if (rv != 0) {
+                    printf("wshell: no such directory: %s\n", dirname);
+                }
             }
         }
         else if (strncmp(input, "echo", 4) == 0)
@@ -45,13 +59,20 @@ int main(int argc, char *argv[])
             if (strncmp(input, "echo ", 5)  == 0 ) {
             char * val = input +5; 
             printf("%s\n", val);
+            } else {
+                printf("\n");
             }
+          
+        }
+          else if (strcmp(input, "pwd") == 0)
+        {
+            printf("%s\n",workingPath);
           
         }
         else
         {
-            system(input);
-            printf("Command not found\n");
+            printf("wshell: could not execute command: %s\n", input);
         }
+        
     }
 }
