98d5ebf0911c76bb1447dcdb4d219c831c58a2aa
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-02-03 04:34:50 +0000

    first part of first

--- project3/project-scheduling/scheduler.c
+++ project3/project-scheduling/scheduler.c
@@ -5,30 +5,30 @@
 #include <string.h>
 #include <limits.h>
 
-
-
 // TODO: Add more fields to this struct
-struct job {
-    int id;
-    int arrival;
-    int length;
-    struct job *next;
+struct job
+{
+  int id;
+  int arrival;
+  int length;
+  struct job *next;
 };
 
-/*** Globals ***/ 
+/*** Globals ***/
 int seed = 100;
 
-//This is the start of our linked list of jobs, i.e., the job list
+// This is the start of our linked list of jobs, i.e., the job list
 struct job *head = NULL;
 
 /*** Globals End ***/
 
 /*Function to append a new job to the list*/
-void append(int id, int arrival, int length){
+void append(int id, int arrival, int length)
+{
   // create a new struct and initialize it with the input data
-  struct job *tmp = (struct job*) malloc(sizeof(struct job));
+  struct job *tmp = (struct job *)malloc(sizeof(struct job));
 
-  //tmp->id = numofjobs++;
+  // tmp->id = numofjobs++;
   tmp->id = id;
   tmp->length = length;
   tmp->arrival = arrival;
@@ -36,47 +36,50 @@ void append(int id, int arrival, int length){
   // the new job is the last job
   tmp->next = NULL;
 
-  // Case: job is first to be added, linked list is empty 
-  if (head == NULL){
+  // Case: job is first to be added, linked list is empty
+  if (head == NULL)
+  {
     head = tmp;
     return;
   }
 
   struct job *prev = head;
 
-  //Find end of list 
-  while (prev->next != NULL){
+  // Find end of list
+  while (prev->next != NULL)
+  {
     prev = prev->next;
   }
 
-  //Add job to end of list 
+  // Add job to end of list
   prev->next = tmp;
   return;
 }
 
-
 /*Function to read in the workload file and create job list*/
-void read_workload_file(char* filename) {
+void read_workload_file(char *filename)
+{
   int id = 0;
   FILE *fp;
   size_t len = 0;
   ssize_t read;
   char *line = NULL,
-       *arrival = NULL, 
+       *arrival = NULL,
        *length = NULL;
 
-  struct job **head_ptr = malloc(sizeof(struct job*));
+  // struct job **head_ptr = malloc(sizeof(struct job*));
 
-  if( (fp = fopen(filename, "r")) == NULL)
+  if ((fp = fopen(filename, "r")) == NULL)
     exit(EXIT_FAILURE);
 
-  while ((read = getline(&line, &len, fp)) > 1) {
+  while ((read = getline(&line, &len, fp)) > 1)
+  {
     arrival = strtok(line, ",\n");
     length = strtok(NULL, ",\n");
-       
-    // Make sure neither arrival nor length are null. 
+
+    // Make sure neither arrival nor length are null.
     assert(arrival != NULL && length != NULL);
-        
+
     append(id++, atoi(arrival), atoi(length));
   }
 
@@ -89,24 +92,54 @@ void read_workload_file(char* filename) {
 }
 
 
-void policy_FIFO(struct job *head) {
+//FIFO policy
+void policy_FIFO(struct job *head, int slice_duration)
+{
+  // TODO: Fill this in
+  printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]\n", slice_duration, head->id, head->arrival, head->length);
+  slice_duration += head->length;
+  if (head->next != NULL)
+  {
+    policy_FIFO(head->next, slice_duration);
+  }
+  return;
+}
+
+void analyze_FIFO(struct job *head)
+{
   // TODO: Fill this in
 
   return;
 }
 
-void analyze_FIFO(struct job *head) {
+//SJF policy
+void policy_SJF(struct job *head, int slice_duration)
+{
+  // TODO: Fill this in
+  printf("t=%d: [Job %d] arrived at [%d], ran for: [%d]\n", slice_duration, head->id, head->arrival, head->length);
+  slice_duration += head->length;
+  if (head->next != NULL)
+  {
+    policy_FIFO(head->next, slice_duration);
+  }
+  return;
+}
+
+void analyze_SJF(struct job *head)
+{
   // TODO: Fill this in
 
   return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
- if (argc < 4) {
+  if (argc < 4)
+  {
     fprintf(stderr, "missing variables\n");
     fprintf(stderr, "usage: %s analysis-flag policy workload-file slice-duration\n", argv[0]);
-		exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
   int analysis = atoi(argv[1]);
@@ -114,13 +147,18 @@ int main(int argc, char **argv) {
        *workload = argv[3];
   int slice_duration = atoi(argv[4]);
 
-  // Note: we use a global variable to point to 
-  // the start of a linked-list of jobs, i.e., the job list 
+  // Note: we use a global variable to point to
+  // the start of a linked-list of jobs, i.e., the job list
   read_workload_file(workload);
 
-  if (strcmp(policy, "FIFO") == 0 ) {
-    policy_FIFO(head);
-    if (analysis) {
+  // FIFO policy
+  if (strcmp(policy, "FIFO") == 0)
+  {
+    printf("Execution trace with FIFO:\n");
+    policy_FIFO(head, slice_duration);
+    printf("End of execution with FIFO.\n");
+    if (analysis)
+    {
       printf("Begin analyzing FIFO:\n");
       analyze_FIFO(head);
       printf("End analyzing FIFO.\n");
@@ -129,7 +167,23 @@ int main(int argc, char **argv) {
     exit(EXIT_SUCCESS);
   }
 
-  // TODO: Add other policies 
+  // SJF policy
+  if (strcmp(policy, "SJF") == 0)
+  {
+    printf("Execution trace with SJF:\n");
+    policy_SJF(head, slice_duration);
+    printf("End of execution with SJF.\n");
+    if (analysis)
+    {
+      printf("Begin analyzing SJF:\n");
+      analyze_SJF(head);
+      printf("End analyzing SJF.\n");
+    }
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
+  }
+
+  // TODO: Add other policies
+
+  exit(EXIT_SUCCESS);
 }
