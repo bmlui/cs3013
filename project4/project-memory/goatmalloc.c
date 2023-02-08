8221714e5354812fc00fbd40b16c86eec22a1d21
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-02-08 18:51:34 +0000

    modified:   project4/project-memory/goatmalloc.c

--- project4/project-memory/goatmalloc.c
+++ project4/project-memory/goatmalloc.c
@@ -1,2 +1,12 @@
 #include <stddef.h>
 #include "goatmalloc.h" // This include should appear last
+
+
+
+void main() {
+    goatmalloc_init();
+    void *ptr = goatmalloc(100);
+    goatfree(ptr);
+    goatmalloc_stats();
+}
+
