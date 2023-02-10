bd4a38b1a1490afd3d8c085b0c3a53cc1d0d10d0
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-02-10 22:24:13 +0000

    l

--- project4/project-memory/goatmalloc.c
+++ project4/project-memory/goatmalloc.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <limits.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <stddef.h>
-#include "goatmalloc.h" // This include should appear last
+#include "goatmalloc.h"
 
 void *_arena_start;
 
@@ -15,7 +21,7 @@ int init(size_t size)
         perror("Error opening /dev/zero");
         return ERR_CALL_FAILED;
     }
-    if (size <= 0 || size > MAX_ARENA_SIZE) // Check for invalid size, account for unsinged  
+    if (size <= 0 || size >= MAX_ARENA_SIZE) // Check for invalid size, account for unsinged  
     {
         return ERR_BAD_ARGUMENTS; // Return error code
     }
@@ -43,15 +49,47 @@ int init(size_t size)
 
 int destroy()
 {
-    if (_arena_start == NULL)
+    if (_arena_start == NULL) // Check for uninitialized arena
     {
         return ERR_UNINITIALIZED;
     }
     int result = munmap(_arena_start, 4096);
-    if (result != 0)
+    if (result != 0) // check for error
     {
         return ERR_SYSCALL_FAILED;
     }
-    printf("Destroying Arena:\n...unmapping arena with munmap()\n");
+    _arena_start = NULL; // set arena to NULL
+    printf("Destroying Arena:\n...unmapping arena with munmap()\n"); 
     return result;
+}
+
+int statusno;
+void* walloc(size_t size)
+{
+    if (_arena_start == NULL) // Check for uninitialized arena
+    {
+          statusno = ERR_UNINITIALIZED;
+          return NULL;
+         
+    }
+    if (size <= 0 || size >= MAX_ARENA_SIZE) // Check for invalid size, account for unsinged  
+    {
+         statusno = ERR_BAD_ARGUMENTS; // Return error code
+        return NULL;
+       
+    }
+   // int page_size = getpagesize();
+    //size = (size + page_size - 1) / page_size * page_size; // Round up to page size
+    node_t *current = _arena_start;
+    while (current != NULL)
+    {
+        if (current->is_free == 1 && current->size >= size)
+        {
+            current->is_free = 0;
+            return current;
+        }
+        current = current->fwd;
+    }
+    statusno = ERR_OUT_OF_MEMORY;
+    return NULL;
 }
